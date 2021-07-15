9d47c6346ceb40027a871ccc6e56a457af73c9c8
Michael <8832013+MichaelYochpaz@users.noreply.github.com>
2021-07-15 20:08:00 +0300

    Added TheWorker-Bypass

---
+++ TheWorker-Bypass.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        TheWorker Bypass
+// @namespace   https://github.com/MichaelYochpaz/User-Scripts
+// @icon        https://www.theworker.co.il/favicon.ico
+// @author      Michael Yochpaz
+// @version     1.0
+// @include     https://theworker.co.il/*
+// @grant       GM_addStyle
+// @description Remove blur and mid screen banner, and enable right-click and selection on theworker.co.il.
+// @downloadURL https://github.com/MichaelYochpaz/User-Scripts/blob/main/TheWorker-Bypass.js
+// @updateURL   https://github.com/MichaelYochpaz/User-Scripts/blob/main/TheWorker-Bypass.js
+// ==/UserScript==
+
+// Enable right-click
+document.onselectstart = undefined;
+document.ondragstart = undefined;
+document.oncontextmenu = undefined;
+document.onkeydown = undefined;
+document.keypress = undefined;
+
+// Remove blur
+GM_addStyle (`.container, .container-salary, .container-interview {filter: none !important; transition: none !important; -webkit-filter: none !important; -webkit-transition: none !important}`);
+
+// Allow selection
+GM_addStyle (`.noselect {-webkit-touch-callout: default !important; -webkit-user-select: auto !important; -khtml-user-select: auto !important; -moz-user-select: auto !important; -ms-user-select: auto !important; user-select: auto !important;}`);
+
+// Remove banner
+document.getElementById("openModal").remove()
