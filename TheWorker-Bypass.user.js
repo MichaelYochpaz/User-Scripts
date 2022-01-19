acca45f6437037f8af0186a844e2c83669e1da54
Michael <8832013+MichaelYochpaz@users.noreply.github.com>
2022-01-19 15:04:22 +0200

    Replace template literals with normal strings

--- TheWorker-Bypass.user.js
+++ TheWorker-Bypass.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/MichaelYochpaz/User-Scripts
 // @icon        https://www.theworker.co.il/favicon.ico
 // @author      Michael Yochpaz
-// @version     1.0.1
+// @version     1.0.2
 // @include     https://theworker.co.il/*
 // @grant       GM_addStyle
 // @description Remove blur and mid screen banner, and enable right-click and selection on theworker.co.il.
@@ -19,10 +19,10 @@ document.onkeydown = undefined;
 document.keypress = undefined;
 
 // Remove blur
-GM_addStyle (`.container, .container-salary, .container-interview {filter: none !important; transition: none !important; -webkit-filter: none !important; -webkit-transition: none !important}`);
+GM_addStyle (".container, .container-salary, .container-interview {filter: none !important; transition: none !important; -webkit-filter: none !important; -webkit-transition: none !important}");
 
 // Allow selection
-GM_addStyle (`.noselect {-webkit-touch-callout: default !important; -webkit-user-select: auto !important; -khtml-user-select: auto !important; -moz-user-select: auto !important; -ms-user-select: auto !important; user-select: auto !important;}`);
+GM_addStyle (".noselect {-webkit-touch-callout: default !important; -webkit-user-select: auto !important; -khtml-user-select: auto !important; -moz-user-select: auto !important; -ms-user-select: auto !important; user-select: auto !important;}");
 
 // Remove banner
 document.getElementById("openModal").remove();
