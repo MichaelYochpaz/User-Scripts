d6e35035927e5b24d0cf567cfebfa02cd4724afe
Michael Yochpaz <8832013+MichaelYochpaz@users.noreply.github.com>
2022-12-10 12:29:27 +0200

    Fix `RealPython-Bypass` user-script not working

--- RealPython-Bypass.user.js
+++ RealPython-Bypass.user.js
@@ -1,16 +1,23 @@
 // ==UserScript==
 // @name        RealPython Registartion Pop-up Bypass
+// @description Remove registration / login pop-up on realpython.com.
 // @namespace   https://github.com/MichaelYochpaz/User-Scripts
+// @homepageURL  https://github.com/MichaelYochpaz/User-Scripts
+// @downloadURL https://raw.githubusercontent.com/MichaelYochpaz/User-Scripts/main/RealPython-Bypass.js
+// @updateURL   https://raw.githubusercontent.com/MichaelYochpaz/User-Scripts/main/RealPython-Bypass.js
+// @supportURL   https://github.com/MichaelYochpaz/User-Scripts/issues
 // @icon        https://cdn.realpython.com/static/favicon.ico
 // @author      Michael Yochpaz
 // @license     MIT
-// @version     1.0.1
-// @include     https://realpython.com/*
-// @description Remove registration / login pop-up on realpython.com.
-// @downloadURL https://raw.githubusercontent.com/MichaelYochpaz/User-Scripts/main/RealPython-Bypass.js
-// @updateURL   https://raw.githubusercontent.com/MichaelYochpaz/User-Scripts/main/RealPython-Bypass.js
+// @version     1.0.2
+// @include     *://realpython.com/*
+// @grant       GM_addStyle
+// @run-at      document-idle
 // ==/UserScript==
 
-// Remove Registration Pop-up
-document.getElementById("rprw").remove();
-document.getElementsByClassName("modal-backdrop").remove();
+// Hide modal
+GM_addStyle (`.modal {display: none !important}`);
+GM_addStyle (`.modal-backdrop.show {opacity: 0 !important}`);
+
+// Restore Scrolling
+GM_addStyle (`.modal-open {overflow: scroll !important}`);
